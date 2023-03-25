47110e92d4132b0ef0599770ea5f1437eedc6511
PathakTushar <tusharph1@gmail.com>
2023-03-25 23:42:08 +0530

    added functionalities through database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -15,15 +16,25 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  
-  Product.create({
-    title: title,
-    description: description,
+
+  req.user.createProduct({
+    title:title,
+    price:price,
     imageUrl: imageUrl,
-    price: price
-  }).then(res => console.log('created product'))
-  .catch(err => console.log(err))
-  
+    description: description,
+  })
+  .then((_) => {
+    console.log("created product")
+    res.redirect('/admin/products')
+  })
+  .catch((err) => console.log(err));
+  // Product.create({
+  //   title: title,
+  //   description: description,
+  //   imageUrl: imageUrl,
+  //   price: price,
+  // })
+
   // const product = new Product(null, title, imageUrl, description, price);
   // product
   //   .save()
@@ -40,17 +51,21 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product,
-    });
-  });
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+    .then((products) => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -59,19 +74,35 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDescription;
+      return product.save();
+    })
+    .then((_) => {
+      console.log("Product Updated");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updatedImageUrl,
+  //   updatedDescription,
+  //   updatedPrice
+  // );
+  // updatedProduct.save();
+  // res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -79,7 +110,7 @@ exports.getProducts = (req, res, next) => {
         path: "/admin/products",
       });
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 };
 
 // exports.getProducts = (req, res, next) => {
@@ -94,6 +125,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+  .then(product =>{
+    return product.destroy();
+  })
+  .then((_)=>{
+    console.log('Product deleted');
+    res.redirect('/admin/products')
+  })
+  .catch (err => console.log(err))
+  // Product.deleteById(prodId);
+  // res.redirect("/admin/products");
 };
--- models/cart.js
+++ models/cart.js
@@ -1,59 +1,76 @@
-const fs = require("fs");
-const path = require("path");
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-const rootDir = require("../util/path");
+const Cart = sequelize.define('cart',{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+})
 
-const p = path.join(rootDir, "data", "cart.json");
+module.exports = Cart;
 
-module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-      const existingProduct = cart.products[existingProductIndex];
-      let updatedProduct;
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty += 1;
-        cart.products[existingProductIndex] = updatedProduct;
-      } else {
-        updatedProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
-      }
-      cart.totalPrice += +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-  static deleteProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) return;
-      const cart = JSON.parse(fileContent);
-      const updatedCart = { ...cart };
-      const product = updatedCart.products.find(prod => prod.id === id)
-      if(!product) return;
-      updatedCart.products = updatedCart.products.filter((prod) => prod.id !== product.id);
-      updatedCart.totalPrice -= productPrice*product.qty;
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-  static getCart(cb){
-    fs.readFile(p, (err,fileContent)=>{
-      if(err){
-        cb(null)
-      }
-      else{
-        const cart = JSON.parse(fileContent);
-        cb(cart);
-      }
-    })
-  }
-};
+
+
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const rootDir = require("../util/path");
+
+// const p = path.join(rootDir, "data", "cart.json");
+
+// module.exports = class Cart {
+//   static addProduct(id, productPrice) {
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart = { products: [], totalPrice: 0 };
+//       if (!err) {
+//         cart = JSON.parse(fileContent);
+//       }
+//       const existingProductIndex = cart.products.findIndex(
+//         (prod) => prod.id === id
+//       );
+//       const existingProduct = cart.products[existingProductIndex];
+//       let updatedProduct;
+//       if (existingProduct) {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty += 1;
+//         cart.products[existingProductIndex] = updatedProduct;
+//       } else {
+//         updatedProduct = { id: id, qty: 1 };
+//         cart.products = [...cart.products, updatedProduct];
+//       }
+//       cart.totalPrice += +productPrice;
+//       fs.writeFile(p, JSON.stringify(cart), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+//   static deleteProduct(id, productPrice) {
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) return;
+//       const cart = JSON.parse(fileContent);
+//       const updatedCart = { ...cart };
+//       const product = updatedCart.products.find(prod => prod.id === id)
+//       if(!product) return;
+//       updatedCart.products = updatedCart.products.filter((prod) => prod.id !== product.id);
+//       updatedCart.totalPrice -= productPrice*product.qty;
+//       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+//         console.log(err);
+//       });
+//     });
+//   }
+//   static getCart(cb){
+//     fs.readFile(p, (err,fileContent)=>{
+//       if(err){
+//         cb(null)
+//       }
+//       else{
+//         const cart = JSON.parse(fileContent);
+//         cb(cart);
+//       }
+//     })
+//   }
+// };
