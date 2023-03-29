a451a02da415f6a684baf02478ca76c23294abe6
PathakTushar <tusharph1@gmail.com>
2023-03-29 23:52:06 +0530

    set up for session and cookies

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require('./routes/auth');
 
 // const Product = require("./models/product");
 const User = require("./models/user");
@@ -45,6 +46,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
